d094b736a34b1df2e12ad4c42863f8f25cc1fd2f
Abhijxxt <abhijeetchatterjee2004@gmail.com>
2025-07-14 18:06:22 +0530

    Renewing everything. Creating Terminal style design. Configures dynamic proejct path entry. Not currently working on CMD need to check

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,4 @@
+# AutoInitiator
 This would be a Python Application, that can be used to automatically create Project files for React/Next/Node
+
+## Currently the system is based on Windows only.
--- __main__.py
+++ __main__.py
@@ -1,11 +1,12 @@
-import os
+import os, sys
+import msvcrt       # For Windows-specific key handling
 
 EXIT_CODE = 0       # 0 - normal, 1 or anything - error
 PROJECT_PATH = ""
 PROJECT_NAME = ""
 GIT_REPOSITORY = ""
-CONFIG_FILE = "./project_path.txt"
-
+CONFIG_FILE = os.getcwd() + "\\project_path.txt"
+# print(CONFIG_FILE)
 def get_project_path():
     global PROJECT_PATH
     if not PROJECT_PATH:
@@ -19,6 +20,54 @@ def get_project_path():
             with open(CONFIG_FILE, "w") as f:
                 f.write(PROJECT_PATH)
 
-get_project_path()
 
+def open_in_vscode():
+    os.system(f"cd {PROJECT_PATH}\\{PROJECT_NAME} && code .")
+    sys.exit(0)
+
+def create_new_project():
+    pass
+
+def change_project_path():
+    PROJECT_PATH = input("Enter project path: ").strip()
+    with open(CONFIG_FILE, "w") as f:
+        f.write(PROJECT_PATH)
+    print(f"Project path changed to: {PROJECT_PATH}")
+
+def terminal_start():
+    print("1. Create a new project")
+    print("2. Change the project path directory")
+    print("3. Show current project path")
+    print("4. Exit")
+
+    choice = msvcrt.getch().decode("utf-8")
+    if choice == '1':
+        os.system("cls")
+        create_new_project()
+    elif choice == '2':
+        os.system("cls")
+        change_project_path()
+    elif choice == '3':
+        os.system("cls")
+        print(f"Current project path: {PROJECT_PATH}")
+    elif choice == '4':
+        terminal_end()
+    else:
+        os.system("cls")
+        print("Invalid choice. Please try again.")
+    
+    terminal_start()
+
+
+def terminal_end():
+    print("Exiting the script. Goodbye!")
+    os.system("pause")
+    sys.exit(0)
+
+## WORKFLOW OF THE SCRIPT
+print("----Welcome to AutoInitiator!----")
+print("This script will help you set up a new project quickly.")
+get_project_path()
+terminal_start()
+terminal_end()
 print(PROJECT_PATH)
